8e22796fa14e55a4ce9762a8137d42f76109a439
Firestorm01X2 <firestorm01x2@gmail.com>
2020-11-23 09:28:18 +0300

    Добавил swagger

---
+++ src/main/java/com/example/lr4/configuration/SwaggerDocumentationConfig.java
@@ -0,0 +1,46 @@
+package com.example.lr4.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.concurrent.CompletionStage;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerDocumentationConfig {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Чат.")
+                .description("Balance.")
+                .license("")
+                .licenseUrl("http://unlicense.org")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .contact(new Contact("", "", ""))
+                .build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.lr4"))
+                .build()
+                .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
+                .apiInfo(apiInfo())
+                .genericModelSubstitutes(CompletionStage.class, ResponseEntity.class);
+    }
+
+}
+
+
