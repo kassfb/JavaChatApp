795a63c25fc3c34491b78beb0b71471a3938d69f
Firestorm01X2 <firestorm01x2@gmail.com>
2020-12-04 09:22:51 +0300

    Initial branch commit

--- src/main/java/com/example/lr4/Lr4Application.java
+++ src/main/java/com/example/lr4/Lr4Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.lr4.models.Messages;
 import com.example.lr4.models.Users;
-import com.example.lr4.services.UserService;
+
 import java.sql.SQLException;
 //throws SQLException // NullPointerException
 
@@ -15,19 +15,19 @@ public class Lr4Application {
 	public static void main(String[] args) throws NullPointerException {
 
 		SpringApplication.run(Lr4Application.class, args);
-		databaseTest();
+		//databaseTest();
 	}
 
-	public static void databaseTest() {
-		UserService userService = new UserService();
-		Users user = new Users("Masha","myStrongPassword");
-		userService.saveUser(user);
-		Messages firstMessage = new Messages("myFirstMessage");//Exception!'org.hibernate.internal.util.config.ConfigurationException: Could not locate cfg.xml resource [hibernate.cfg.xml]
-		firstMessage.setUserId(user);
-		user.addMessage(firstMessage);
-		Messages secondMessage = new Messages("mySecondMessage");
-		secondMessage.setUserId(user);
-		user.addMessage(secondMessage);
-		userService.updateUser(user);
-	}
+//	public static void databaseTest() {
+//		UserService userService = new UserService();
+//		Users user = new Users("Masha","myStrongPassword");
+//		userService.saveUser(user);
+//		Messages firstMessage = new Messages("myFirstMessage");//Exception!'org.hibernate.internal.util.config.ConfigurationException: Could not locate cfg.xml resource [hibernate.cfg.xml]
+//		firstMessage.setUserId(user);
+//		user.addMessage(firstMessage);
+//		Messages secondMessage = new Messages("mySecondMessage");
+//		secondMessage.setUserId(user);
+//		user.addMessage(secondMessage);
+//		userService.updateUser(user);
+//	}
 }
---
+++ src/main/java/com/example/lr4/models/MessagesRepository.java
@@ -0,0 +1,6 @@
+package com.example.lr4.models;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessagesRepository extends CrudRepository<Messages, Integer> {
+    Messages findById(int id);
+}
